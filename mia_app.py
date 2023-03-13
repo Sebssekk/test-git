6c3a64f82f95956989c285ff9dd048d43d352ad4
Sebastiano Secchiaroli <sebssekk@hotmail.it>
2023-03-13 16:02:00 +0100

    step 2

--- mia_app.py
+++ mia_app.py
@@ -1,2 +1,6 @@
-print("il mio codice sdgoas sgfiaso asdio hdspofuasp0 sduhagopd ")
+print("il fe mio codice sdgoas sgfiaso asdio hdspofuasp0 sduhagopd ")
 print("dgffdfaos")
+var = 2
+
+for o in range(3):
+    print("boh")
